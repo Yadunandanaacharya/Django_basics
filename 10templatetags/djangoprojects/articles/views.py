e9d4c31908d6da157f97ad6137b1222ea0076e0d
Yadunandana Acharya <51967645+Yadunandanaacharya@users.noreply.github.com>
2020-09-09 17:37:01 +0530

    Django project

--- 10templatetags/djangoprojects/articles/views.py
+++ 10templatetags/djangoprojects/articles/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Article
 # Create your views here.
 def article_list(request):
-    article1 = Article.objects.all().order_by('date')
-    return render(request, 'articles/article_list.html',{'art':article1})
+    article = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html',{'artticles':article})
